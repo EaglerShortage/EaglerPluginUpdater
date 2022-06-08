bbc16d209e23f149f9775b2d3d5b2a85e37f2b77
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-08 12:51:13 -0700

    recompiled

--- src/main/java/tech/nully/PluginInstaller/Main.java
+++ src/main/java/tech/nully/PluginInstaller/Main.java
@@ -5,7 +5,6 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class Main extends JavaPlugin {
     private static Plugin instance = null;
@@ -16,18 +15,15 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         instance = this;
         try {
-            Installer.InstallUpdater();
-        } catch (IOException e) {}
-        getCommand("install").setExecutor(new InstallCommand());
-        getCommand("plist").setExecutor(new plistCommand());
+            Updater.UpdateInstaller();
+        } catch (IOException e) {
+            System.out.println("PluginUpdater: Error while updating PluginInstaller, please contact Bongo Cat#1100 on discord");
+        }
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage(
-                ChatColor.GREEN + "[EaglerPluginInstaller]" + ChatColor.AQUA + " EaglerPluginInstaller V1.0.9 is now Enabled! :D");
-        getServer().getConsoleSender().sendMessage("To use PluginInstaller, run the command \"/install <pluginname>\" (\"install <pluginname>\" in console) to install a plugin");
-        getServer().getConsoleSender().sendMessage("To get a list of installable plugins, use the command \"/plist\" or \"plist\" in console");
-        getServer().getConsoleSender().sendMessage("You can request to add a new plugin to the database by going to https://github.com/darverdevs/PluginInstallerRepo/tree/main");
-        getServer().getConsoleSender().sendMessage("and creating an issue or pull request of the plugin you want to add");
+                ChatColor.GREEN + "[EaglerPluginUpdater]" + ChatColor.AQUA + " EaglerPluginUpdater V1.0.9 is now Enabled! :D");
+        getServer().getConsoleSender().sendMessage("You do not need to do anything, this plugin will automatically update your PluginUpdater on startup");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
     }
@@ -37,7 +33,7 @@ public class Main extends JavaPlugin {
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage(
-                ChatColor.GREEN + "[EaglerPluginInstaller]" + ChatColor.AQUA + " PluginInstaller V1.0.9 is now Disabled! D:");
+                ChatColor.GREEN + "[EaglerPluginUpdater]" + ChatColor.AQUA + " EaglerPluginUpdater V1.0.8 is now Disabled! D:");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
         getServer().getConsoleSender().sendMessage("--------------------------------------------");
     }
---
+++ src/main/java/tech/nully/PluginInstaller/Updater.java
@@ -0,0 +1,22 @@
+package tech.nully.PluginInstaller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class Updater {
+    public static void UpdateInstaller() throws IOException {
+        InputStream in = URI.create("https://github.com/darverdevs/PluginInstaller/raw/main/out/artifacts/EaglerPluginInstaller_jar/EaglerPluginInstaller.jar")
+                .toURL().openStream();
+        File f = new File(Main.getInstance().getDataFolder().getParent() + "/EaglerPluginInstaller.jar");
+        f.delete();
+        Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        getServer().getPluginManager().disablePlugin(tech.nully.PluginInstaller.Main.getInstance());
+        getServer().getPluginManager().enablePlugin(tech.nully.PluginInstaller.Main.getInstance());
+    }
+}
