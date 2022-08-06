802b7809dd99f3cf6be1d38ee0091fcacc1c556b
BongoCat <77935781+Fangoboyo@users.noreply.github.com>
2022-08-06 13:53:39 -0700

    Chnaged the link path of EaglerPluginInstaller

--- src/main/java/tech/nully/PluginUpdater/Updater.java
+++ src/main/java/tech/nully/PluginUpdater/Updater.java
@@ -1,20 +1,20 @@
 package tech.nully.PluginUpdater;
 
+import org.bukkit.plugin.Plugin;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import tech.nully.PluginInstaller.Main;
 
 import static org.bukkit.Bukkit.getServer;
 
-import org.bukkit.plugin.Plugin;
-import tech.nully.PluginInstaller.Main;
-
 public class Updater {
     public static void UpdateInstaller() throws IOException {
-        InputStream in = URI.create("https://github.com/darverdevs/PluginInstaller/raw/main/out/artifacts/EaglerPluginInstaller_jar/EaglerPluginInstaller.jar")
+        InputStream in = URI.create("https://github.com/darverdevs/PluginInstaller/raw/main/target/EaglerPluginInstaller-1.1.5.jar")
                 .toURL().openStream();
         File f = new File(Main.getInstance().getDataFolder().getParent() + "/EaglerPluginInstaller.jar");
         Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
